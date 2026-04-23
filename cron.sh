cbdcc9b8a15e8be1686183ed52b0d53604fd631e
charles.lee <cheoljoo.lee@lge.com>
2026-04-23 14:35:20 +0900

    -

--- cron.sh
+++ cron.sh
@@ -1,5 +1,14 @@
 # change your id , passwd in myidpasswd.py file
 cd /home/cheoljoo.lee/code/crontab/weekly_report_4_jira; source /home/cheoljoo.lee/code/problemSolving/2022/a/bin/activate ; make > /home/cheoljoo.lee/code/crontab/weekly_report_4_jira/cron.log 2>&1
+
+grep "retry count: 10 start: 0 1 cheoljoo.lee" /home/cheoljoo.lee/code/crontab/weekly_report_4_jira/cron.log
+if [ $? -eq 0 ]
+then
+    python3 /home/cheoljoo.lee/code/crontab/weekly_report_4_jira/sendmail.py --sender="cheoljoo.lee@lge.com"  --logfile="/home/cheoljoo.lee/code/crontab/weekly_report_4_jira/cron.log"
+else
+    echo "no error : weekly_report_4_jira"
+fi
+
 grep "Traceback (most recent call last)" /home/cheoljoo.lee/code/crontab/weekly_report_4_jira/cron.log
 if [ $? -eq 0 ]
 then
